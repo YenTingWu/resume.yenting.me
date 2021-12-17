d4d32b5a6612fc54fae088457c79c827a808bc73
YenTingWu <a9600125a@gmail.com>
2021-12-17 22:45:21 +0800

    cut blank page in pdf version with puppeteer

--- pdf.js
+++ pdf.js
@@ -1,20 +1,31 @@
 const puppeteer = require("puppeteer");
 
+const MY_SERVER = "http://127.0.0.1:5500";
+const FILE = "index.html";
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("http://127.0.0.1:5500/index.html", {
+  await page.goto(`${MY_SERVER}/${FILE}`, {
     waitUntil: "networkidle2",
   });
 
-  await wait();
+  await page.evaluate(() => {
+    const container = document.getElementById("container");
+    const footer = document.querySelector("footer");
+    if (footer) {
+      container.removeChild(footer);
+    }
+  });
 
   await page.pdf({
     path: "resume.pdf",
     format: "A4",
     displayHeaderFooter: false,
     printBackground: true,
+    // To cut the blank page, if the content is over one page, should remove this property
+    pageRanges: "1",
     margin: {
       top: "0.3in",
       bottom: "0.3in",
@@ -25,8 +36,3 @@ const puppeteer = require("puppeteer");
 
   await browser.close();
 })();
-
-const wait = async () =>
-  new Promise((resolve) => {
-    setTimeout(resolve, 3000);
-  });
